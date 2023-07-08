8f86b1f69d5fef05b8b345117b9a9f0cfa79ae7b
Talha Qamar <qamartalha@protonmail.com>
2023-07-08 14:56:04 +0500

    Added public interface

--- app/Component.hs
+++ app/Component.hs
@@ -1,23 +1,51 @@
-module Component where
-import Data.Fixed (E0)
+module Component(
+  Component,
+  ComponentType,
+  setUnion,
+  getUnionValue,
+  initComponentFromType,
+  setStructElement,
+  getStructElement
+)where
 
+
+-- public interface
+
+
+newtype Component = Component { getComp :: Comp } deriving Show
+
+setUnion :: Component -> String -> Component -> Component
+setUnion union variant value = Component $ setUnionComp (getComp union) variant (getComp value)
+
+getUnionValue :: Component -> Component
+getUnionValue = Component . getUnionCompValue . getComp
+
+initComponentFromType :: ComponentType -> Component
+initComponentFromType = Component . initCompFromType
+
+setStructElement :: Component -> String -> Component -> Component
+setStructElement struct element value = Component $ setStructCompElement (getComp struct) element (getComp value)
+
+getStructElement ::Component -> String -> Component 
+getStructElement struct element = Component $ getStructCompElement (getComp struct) element
 data ComponentType = IntegerType | BooleanType | StructType String [(String, ComponentType)] | UnionType String [(String, ComponentType)] deriving (Show, Eq)
 
+
+-- private interface
 data CompData = Integer Int | Boolean Bool | Struct [(String, Comp)] | Union String Comp deriving Show
 
 data Comp = Comp ComponentType CompData deriving Show
 
-newtype Component = Component { getComp :: Component } deriving Show
 
-setUnion :: Comp -> String -> Comp -> Comp
-setUnion (Comp (UnionType x typelist) (Union _ _)) variant (Comp comptype compdata) 
+setUnionComp :: Comp -> String -> Comp -> Comp
+setUnionComp (Comp (UnionType x typelist) (Union _ _)) variant (Comp comptype compdata) 
   | (variant, comptype) `elem` typelist =
     Comp
       (UnionType x typelist)
       (Union variant (Comp comptype compdata))
 
-getUnionValue :: Comp -> Comp
-getUnionValue (Comp _ (Union _ x)) = x
+getUnionCompValue :: Comp -> Comp
+getUnionCompValue (Comp _ (Union _ x)) = x
 
 
 initCompFromType :: ComponentType -> Comp
@@ -29,14 +57,16 @@ initCompFromType (UnionType name list) = Comp (UnionType name list) (Union headt
     headtype = fst $ head list
     headcomp = initCompFromType $ snd $ head list
 
-setStructElement :: Comp -> String -> Comp -> Comp
-setStructElement (Comp (StructType name types) (Struct comps)) element (Comp _type value)
+setStructCompElement :: Comp -> String -> Comp -> Comp
+setStructCompElement (Comp (StructType name types) (Struct comps)) element (Comp _type value)
   | (element, _type) `elem` types = 
     Comp 
       (StructType name types) 
       (Struct  $ (element , Comp _type value) : filter (\(x, y) -> x /= element) comps)
 
-getStructElement :: Comp -> String -> Comp
-getStructElement (Comp _ (Struct list)) element
+getStructCompElement :: Comp -> String -> Comp
+getStructCompElement (Comp _ (Struct list)) element
   | element `elem` ( map fst list ) = snd $ head $ filter (\(x, _) -> x == element) list   
+
+
 
